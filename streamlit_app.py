7bd593f72d41ae04a589bb1c8aca41b46ac8b836
filipe <filipe.sa1@ibm.com>
2023-12-03 11:27:29 +0000

    xxxx

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-print('it works')
+import cv2
+import streamlit as st
+import numpy as np
+
+st.title("OpenCV Demo App")
+st.subheader("This app allows you to play with Image filters!")
+st.text("We use OpenCV and Streamlit for this demo")
